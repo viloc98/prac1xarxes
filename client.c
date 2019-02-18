d3bb3065423e8b5b754c29dc1519efabe6da5275
Víctor Serrano <viloc98@gmail.com>
2019-02-18 19:08:56 +0100

    Millorada funció lectura fitxer i afegit fitxer per defecte.

--- client.c
+++ client.c
@@ -3,7 +3,16 @@
 #include <string.h>
 #define DIMENSION       100 /* LA DIMENSIO INICIAL DELS STRINGS PER A DADES DEL CLIENT */
 
-char *fitxer_a_llegir;
+
+
+#define REGISTER_REQ  0x00
+#define REGISTER_ACK  0x01
+#define REGISTER_NACK  0x02
+#define REGISTER_REJ  0x03
+#define ERROR  0x09
+
+
+char *file_to_read;
 struct info_client
 {
 	char nom[DIMENSION];
@@ -16,56 +25,54 @@ struct info_client client;
 void llegirArguments(int argc, char const *argv[])
 {
 	int i;
+	/*S'asignen valors per defecte en cas de que no es passin arguments*/
+	file_to_read = "client.cfg";
 	for (i = 0; i < argc; ++i)
 	{
 		if (strcmp(argv[i],"-c")==0)
 		{
-			fitxer_a_llegir = (char *) malloc(strlen(argv[i+1]));
-			strcpy(fitxer_a_llegir, argv[i+1]);
-			printf("%s\n", fitxer_a_llegir);
+			file_to_read = (char *) malloc(strlen(argv[i+1]));
+			strcpy(file_to_read, argv[i+1]);
+			printf("%s\n", file_to_read);
 		}
 	}
 }
 
-void llegirFitxerCfg()
+void readFile()
 {
 	char line[256];						                /* linia */
 	int line_num = 0;					                /* numero de linia */
 	char resultat[4][256];	/* nom i valor del parametre els iguals els ignorem*/
-	int k=0;
 	int i=0;
 	int j=0;
 	int x=0;
 	FILE * f;
-	f = fopen(fitxer_a_llegir, "r");
-
+	f = fopen(file_to_read, "r");
 	if (f==NULL) {fputs ("File error",stderr); exit (1);}
 	while(fgets(line, 256, f) != NULL){
-		j=0;
-		for (i=0;i<256;i++)
+		for (i=0;line[i];i++)
 		{
-			if (line[i]==' ')
-			{
-				for (x=i+1; x<256 && line[x] != '\n';x++)
-				{
-					resultat[k][j]=line[x];
-					j++;
-				}
-        resultat[k][j] = '\0';
-				k++;
-			}
+		    if (line[i]==' ')
+		    {
+		        j=0;
+		        for (x=i+1; x<256 && line[x] != '\n';x++)
+		        {
+		            resultat[line_num][j]=line[x];
+		            j++;
+		        }
+		        resultat[line_num][j] = '\0';
+		        line_num++;
+		        i=x;
+		    }
 		}
-		line_num++;
 	}
-	fclose(f);
 
+	fclose(f);
 	/*Anem copiant la informació del client*/
 	strcpy(client.nom, resultat[0]);
 	strcpy(client.mac, resultat[1]);
 	strcpy(client.server_IP, resultat[2]);
 	strcpy(client.server_port, resultat[3]);
-
-	printf("Llegits parametres arxius de configuració.\n");
 
   printf("%s\n", client.nom);
   printf("%s\n", client.mac);
@@ -76,6 +83,6 @@ void llegirFitxerCfg()
 int main (int argc, char const *argv[])
 {
 	llegirArguments(argc, argv);
-	llegirFitxerCfg();
+	readFile();
   exit(0);
 }
